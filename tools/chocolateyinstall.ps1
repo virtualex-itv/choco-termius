366abcfb03991d751a300a9c9161f8e25ebfbadb
Alex Lopez <info@ithinkvirtual.com>
2020-03-25 14:02:51 -0400

    Updated to 5.6.2

--- tools/chocolateyinstall.ps1
+++ tools/chocolateyinstall.ps1
@@ -3,12 +3,9 @@ $toolsDir	= "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 $packageName	= 'termius'
 $fileType	= 'exe'
 $url	= 'https://autoupdate.termius.com/win/Termius.exe'
-$url64bit	= $url
 $softwareName	= 'Termius*'
-$checksum	= '65664DE09780AC9CE36B8ABCA31A0A5B91701FCCA016FF1B3AD99E01C33E5C3A'
+$checksum	= '6226BE8763CC9F1E9E6F0D5FB558B301FA1CE14B8199E188AA97E5BD8B413ACC'
 $checksumType	= 'sha256'
-$checksum64	= $checksum
-$checksumType64	= $checksumType
 $silentArgs	= '/S'
 $validExitCodes		= @(0)
 
@@ -16,12 +13,9 @@ $validExitCodes		= @(0)
 	  packageName   	= $env:ChocolateyPackageName
 	  fileType			= $fileType
 	  url				= $url
-	  url64bit			= $url64bit
 	  unzipLocation 	= $toolsDir
 	  checksum      	= $checksum
 	  checksumType		= $checksumType
-	  checksum64      	= $checksum64
-	  checksumType64	= $checksumType64
 	  silentArgs		= $silentArgs
 	  validExitCodes	= $validExitCodes  
 	}
